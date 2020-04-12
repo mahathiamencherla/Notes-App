57189eb76b294f82bb3d73e4ffd4c9b763583244
mahathiamencherla15 <mahathiamencherla15@gmail.com>
2020-04-13 00:49:06 +0530

    CommandLine Arguments using Yargs

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require("./notes.js")
 
-const command = process.argv[2]
 
-if(command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log("Removing Note!")
-}
+//customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Notes --> add, remove, read, list
+
+//create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true, // Won't run without providing -> --title="Note Title"
+            type: 'string'
+        
+        },
+        body: {
+            describe: "Body of the note",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log("Adding a new note with title: "+ argv.title)
+        console.log(argv.body)
+    }
+})
+
+//create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note',
+    handler: function () {
+        console.log("Removing a note!")
+    }
+})
+
+//create list command
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function () {
+        console.log("Listing all notes!")
+    }
+})
+
+//create read command
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function () {
+        console.log("Reading a note!")
+    }
+})
+
+yargs.parse()
+//console.log(yargs.argv)
